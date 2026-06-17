be67e820c59ba6bbe6f8356e1663967ee6aac2d0
sudeep-star <asudip63@gmail.com>
2026-06-17 14:54:19 +0545

    Add Parametrize in test methods

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,6 +3,11 @@ import pytest
 
 @pytest.mark.smoke
 @pytest.mark.regression
+#@pytest.mark.parametrize(
+    #"username,password",[
+        #("standard_user","secret_sauce"),
+        #("locked_out_user","secret_sauce"),
+        ##])
 def test_valid_login(driver):
     login_page=LoginPage(driver)
 
@@ -11,24 +16,35 @@ def test_valid_login(driver):
     login_page.login(
         "standard_user",
         "secret_sauce")
+    login_page.login()
     
     assert "inventory" in driver.current_url
 
 
 @pytest.mark.regression
-def test_invalid_login(driver):
+@pytest.mark.parametrize(
+    "username,password",[
+        ("",""),
+        ("standard_user",""),
+        ("","secret_sauce"),
+        
+    ])
+@pytest.mark.sanity
+def test_invalid_login(driver,username,password):
     login_page=LoginPage(driver)
 
     login_page.open()
 
-    login_page.login(
-        "wrong_username",
-        "wrong_password"
-    )
+    #login_page.login(
+        #"wrong_username",
+        #"wrong_password"
+    #)
+    login_page.login(username,password)
 
-    assert (
-        "Username and password"
-        in login_page.get_error_message())
+    #assert (
+        #"Username and password"
+        #in login_page.get_error_message())
+    assert login_page.get_error_message() != ""
     
 @pytest.mark.regression    
 def test_locked_out_user(driver):
